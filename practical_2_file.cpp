481bf2a1214da4bbfde539d4f6ce3abd44992c08
Arshad Bagwan <arshadbagwan9373@gmail.com>
2025-05-04 11:23:58 +0530

    Update practical_2_file.cpp

--- practical_2_file.cpp
+++ practical_2_file.cpp
@@ -1,101 +1,122 @@
 #include<iostream>
-#include<string.h>
 #include<fstream>
+#include<string.h>
 using namespace std;
 
+void insert_record(string r , string n,string d ,string a){
 
-
-void insert(){
-        fstream f1;
-	string name;
-	int roll;
-	f1.open("sample.txt",ios::app);
-	cout<<"Enter Details:"<<endl;
-	cout<<"Enter Name :";
-	cin>>name;
-	cout<<"Enter Roll No :";
-	cin>>roll;
-	f1<<name<<"  "<<roll<<endl;
-	f1.close();
-
-	 }
+    fstream f;
+    string name,roll,addr,div;
+    f.open("sample.txt",ios::app);
+    f<<r<<" "<<n<<" "<<d<<" "<<a<<endl;
+    f.close();
+};
 
 
 void display(){
-        fstream f2;
-	f2.open("sample.txt",ios::in);
-	string s1;
-	while(!f2.eof()){
-	getline(f2,s1);
-	cout<<s1<<endl;
-	}
-	f2.close();
-  }
-  
- //--------------- 
-void remove(string roll){
+    fstream f;
+    string name,roll,addr,div;
+    f.open("sample.txt",ios::in);
+    while (f >> roll >> name >> div >> addr ) {
 
-fstream f3,f2;
-f2.open("sample.txt",ios::in);
-f3.open("new.txt",ios::out);
-
-while(!f2.eof()){
-string str;
-getline(f2,str);
-if (str.find(roll)>=50)
-{ f3<<str<<endl;}
-}
-f2.close();
-f3.close();
-
-f2.open("sample.txt",ios:: out);
-f3.open("new.txt",ios::in);
-
-while(!f3.eof()){
-string str;
-getline(f3,str);
-f2<<str<<endl;
+    cout << "Roll No.: " << roll << "\n";
+    cout << "Name: " << name << "\n";
+    cout << "Division: " << div << "\n";
+    cout<<"Address :"<<addr<<'\n';
+    cout << "--------------------------\n";
+    }
+    f.close();
 
 }
-f2.close();
-f3.close();
+
+void remove_record(string roll){
+    ifstream f("sample.txt");
+    ofstream f2("temp.txt");
+    string n,r,a,d,line;
+    while (f >> r >> n >> d >> a ) {
+            if (roll !=r){
+                f2<<r<<" "<<n<<" "<<d<<" "<<a<<endl;               
+            }
+        }
+    f.close();
+    f2.close();
+    remove("sample.txt");
+    rename("temp.txt","sample.txt");
+    cout<<"Record deleted Succesffully....."<<endl;
+    
 }
-//--------------
+
+void search_record(string roll){
+    fstream f;
+    int flag=0;
+    string r,n,d,a;
+    f.open("sample.txt",ios::in);
+    while(f>>r>>n>>d>>a){
+        if (r==roll){
+            cout<<"Record Found !!"<<endl;
+            cout << "Roll No.: " << r << "\n";
+            cout << "Name: " << n << "\n";
+            cout << "Division: " << d << "\n";
+            cout<<"Address :"<<a<<'\n';  
+            flag=1; 
+        }    
+    }
+    if (flag==0){ cout<<"Record not present";}
+
+}
 
 int main(){
-	int ch=0;
-	int n;
-	
-	while(ch<3){
-	cout<<"**** File Handling ****"<<endl;
-	cout<<"1.Insert details"<<endl;
-	cout<<"2.Show Details"<<endl;
-	cout<<"3.remove record"<<endl;
-	cout<<"4.Exit"<<endl;
-	cout<<"Enter Your Choice :";
-	cin>>ch;
-	
-	if(ch==1){
-	cout<<"Enter No.of Students :";
-	cin>>n;
-	for(int i=0;i<n;i++){
-	insert();}
-	}
-	if(ch==2){
-	display();
-	}
-	if(ch==3){
-	string r;
-	 cout<<"Enter roll number to remove :";
-	 cin>>r;
-	 remove(r);
-	}
-	if(ch==4){
-		break;
-	}
-	
-	}
+    while(true){
 
+        int ch;
+        string name,roll,addr,div;
+        cout<<"1.insert Record"<<endl;
+        cout<<"2.Delete Record"<<endl;
+        cout<<"3.Display Record"<<endl;
+        cout<<"4.Search Record"<<endl;
+        cout<<"5.Exit"<<endl;
+        cout<<"Enter your Choice :";
+        cin>>ch;
+        
+        switch(ch){
+            case 1:
+                cout<<"Enter Roll_No :";
+                cin>>roll;
+                cout<<"Enter Name :";
+                cin>>name;
+                cout<<"Enter Division :";
+                cin>>div;
+                cout<<"Enter Address :";
+                cin>>addr;
+                insert_record(roll,name,div,addr);
+                break;
+    
+            case 2:
+                cout<<"Enter Roll_No :";
+                cin>>roll;
+                remove_record(roll);
+                break;
+            
+            case 3:
+                display();
+                break;
+    
+            case 4:
+                cout<<"Enter Roll_No :";
+                cin>>roll;
+                search_record(roll);
+                break;
+            default :
+                cout<<" !!! Invalid Choice !!!";
 
-		return 0;
+            case 5:
+                exit(0);
+                break;
+    
         }
+
+
+    }
+    
+    return 0;
+}
