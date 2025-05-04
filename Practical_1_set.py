d35a0bb31bce388836b982a58cf13b389a496d6d
Arshad Bagwan <arshadbagwan9373@gmail.com>
2025-05-04 23:36:12 +0530

    Update Practical_1_set.py

--- Practical_1_set.py
+++ Practical_1_set.py
@@ -1,83 +1,117 @@
-from SetOperations import Set
-def createSet():
-	n=int(input("Enter Number of Elements in Set "))
-	s=Set(n)
-	return s
-choice=0
-print("Creating Set")
-s1=createSet()
-print(str(s1))
-while choice!=10:
-	print("Menu")
-	print("1.Add")
-	print("2.Remove")
-	print("3.Contains")
-	print("4.Size")
-	print("5.Intersection")
-	print("6.Union")
-	print("7.Difference")
-	print("8.Subset")
-	print("9.Proper Subset")
-	print("10.Exit")
-	choice=int(input("Please Enter Your Choice "))
-	if choice==1:
-		e=int(input("Enter Number To Add "))
-		s1.add(e)
-		print(str(s1))
-	elif choice==2:
-		e=int(input("Enter Number To Remove "))
-		s1.remove(e)
-		print(str(s1))
-	elif choice==3:
-		e=int(input("Enter Number to Search "))
-		if e in s1:
-			print("Number is present in Set")
-		else:
-			print("Number is not present In Set")
-	elif choice==4:
-		print("Set contains {} Elements".format(len(s1)))
-	elif choice==5:
-		print("Create a Set B for Intersection Operation")
-		s2=createSet()
-		s3=s1.intersect(s2)
-		print("Set A="+str(s1))
-		print("Set B="+str(s2))
-		print("Intersection="+str(s3))
-	elif choice==6:
-		print("Create a Set B for Union Operation")
-		s2=createSet()
-		s3=s1.union(s2)
-		print("set A="+str(s1))
-		print("Set B="+str(s2))
-		print("Union="+str(s3))
-	elif choice==7:
-		print("Create a set B for calculating Set Difference")
-		s2=createSet()
-		s3=s1.difference(s2)
-		print("Set A="+str(s1))
-		print("Set B="+str(s2))
-		print("Difference="+str(s3))
-	elif choice==8:
-		print("Create a Set B for checking Subset")
-		s2=createSet()
-		isSubset=s1.isSubsetOf(s2)
-		print("Set A="+str(s1))
-		print("Set B="+str(s2))
-		if isSubset:
-			print("Set B is a Subset of Set A")
-		else:
-			print("Set B is not a Subset Of set A")
-	elif choice==9:
-		print("Create A set B for checking Proper Subset")
-		s2=createSet()
-		isProperSubset=s1.isProperSubset(s2)
-		print("Set A="+str(s1))
-		print("Set B="+str(s2))
-		if isProperSubset:
-			print("Set B is a Proper Subset of Set A")
-		else:
-			print("Set B is not A Proper Subset of Set A")
-	elif choice==10:
-		break
-	else:
-		print("Please Enter A Valid Choice")
+class set:
+    s1=[]
+    s2=[]
+    
+
+    def contains(self,l,key=0):
+        if key in l :
+            return True
+        else:
+            return False
+        
+    
+    def add_element(self):
+        n = int(input("Enter Number of element in set 1 :"))
+        for i in range(0,n):
+            self.s1.append(int(input("Enter element :")))
+
+        n = int(input("Enter Number of element in set 2 :"))
+        for i in range(0,n):
+            self.s2.append(int(input("Enter element :")))
+
+
+    def remove_element(self,val):
+        self.s1.remove(val)
+
+    def size(self):
+        count=1
+        for i in self.s1:
+            count+=1
+        return count
+    
+    def intersection(self,l1,l2):
+        l3 = []
+        for i in l2:
+            if i in l1:
+                l3.append(i)
+        return l3
+    
+    def union(self,l1,l2):
+        l3= []
+        for i in l1:
+            if i not in l3:
+                l3.append(i)
+        for i in l2:
+            if i not in l3:
+                l3.append(i)
+        return l3
+    
+    def difference(self,l1,l2):
+        l3 = []
+        for i in l1:
+            if i not in l2:
+                l3.append(i)
+        return l3
+    
+    def subset(self,l1,l2):
+        flag = 0
+        for i in l1:
+            if i in l2:
+                continue
+            else:
+                flag=1
+                break
+        if flag==0:
+            return True
+        else:
+            return False
+    
+
+s = set()
+def menu():
+    print("----- MENU --------")
+    print("1.Add Element")
+    print("2.Delete Element")
+    print("3.Contains")
+    print("4.size")
+    print("5.Intersection")
+    print("6.Union")
+    print("7.Difference")
+    print("8.Subset")
+    ch = int(input("Enter your Choice :"))
+
+    match ch:
+        case 1 :
+            
+            s.add_element()
+
+        case 2:
+            ele = int(input("Enter element :"))
+            s.remove_element(ele)
+
+        case 3:
+            ele = int(input("Enter element :"))
+            print(s.contains(s.s1,ele))
+        
+        case 4:
+            print("size = " ,s.size())
+
+        case 5:
+           print("Intersection = " ,s.intersection(s.s1,s.s2)) 
+
+        case 6:
+            print("union = " ,s.union(s.s1,s.s2))
+
+        case 7:
+            print("Difference = " ,s.difference(s.s1,s.s2))
+
+        case 8:
+            print("Subset =" ,s.subset(s.s1,s.s2))
+
+        case 9:
+            exit()
+
+
+while(True):
+    menu()
+
