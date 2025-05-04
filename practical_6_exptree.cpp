2ca80f8f7a6ba074c825e435c5056e703b448dfd
Arshad Bagwan <arshadbagwan9373@gmail.com>
2025-05-05 01:58:07 +0530

    Update practical_6_exptree.cpp

--- practical_6_exptree.cpp
+++ practical_6_exptree.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<string>
-#define MAX 50
+#include<stack>
 using namespace std;
 
 struct Node{
@@ -13,26 +13,14 @@ Node *right;
 
 class tree{
    public:
-   int top;
    string exp;
-   Node *stack[MAX];
+   stack<Node*> st;
    
-   tree(){
-       top=-1;
-   }
-   void push(Node *ptr){
-       stack[++top]=ptr;
-       cout<<"Operand (Pushed to stack)"<<endl;
-   }
    
-   Node *pop(){
-       return stack[top--];
-   }
-   
-   void accept(){
+   Node *accept(){
        cout<<"Enter Expression :";
        cin>>exp;
-       int s=size(exp)-1;
+       int s=exp.length()-1;
        for(int j=s;j>=0;j--){
            node = new Node;
            node->data=exp[j];
@@ -40,40 +28,58 @@ class tree{
            if(isalpha(exp[j])){
                node->left=NULL;
                node->right=NULL;
-               push(node);
+               st.push(node);
                
            }
            else{
-               Node *n1;
-               Node *n2;
-               n1=pop();
-               n2=pop();
-               node->right=n1;
-               node->left=n2;
-               push(node);
+               node->left=st.top(); st.pop();
+               node->right=st.top(); st.pop();
+               st.push(node);
                
            }
        }
        
-       
+      return st.top(); 
    }
    
-   void display(){
-       
-       for(int i=0;i<=top;i++){
-           cout<<stack[i]->data<<endl;
-       }
-       
-   }
-   
-   void printPostorder(Node* ptr)
+   void display(Node* root)
 {
-    if (ptr == NULL)
-        return;
+    
+    stack<Node*> s1,s2;
+    s1.push(root);
 
-    printPostorder(ptr->left);
-    printPostorder(ptr->right);
-    cout << ptr->data << " ";
+    while(!s1.empty()){
+        
+        Node *node = s1.top();s1.pop();
+        s2.push(node);
+
+        if (node->left) s1.push(node->left);
+        if (node->right) s1.push(node->right);
+
+    }
+
+    while (!s2.empty())
+    {
+        cout<<s2.top()->data<<" ";
+        s2.pop();
+    }
+    
+
+}
+
+void deletetree(Node *ptr){
+    stack<Node*>s;
+    s.push(ptr);
+
+    while(!s.empty()){
+        node = s.top() ; s.pop();
+
+        if (node->left) s.push(node->left);
+        if (node->right) s.push(node->right);
+        delete node;
+    }
+
+    cout<<"Tree deleted Succesfully...."<<endl;
 }
    
 };
@@ -81,10 +87,32 @@ class tree{
 int main(){
     
     tree t;
-    t.accept();
-    t.display();
-    t.printPostorder(t.pop());
+    int ch;
+    Node *r;
+    while(1){
+    cout<<"\n----- MENU ----"<<endl;
+    cout<<"1.Insert Data"<<endl;
+    cout<<"2.Display Postorder"<<endl;
+    cout<<"3.Delete Tree"<<endl;
+    cout<<"4.Exit"<<endl;
+    cout<<"Enter Choice :--";
+    cin>>ch;
 
+    switch(ch){
+        case 1:
+            r =t.accept();
+            break;
+        case 2:
+            t.display(r);
+            break;
+
+        case 3:
+            t.deletetree(r);
+            break;
+        case 4:
+           exit(0);
+    }
+}
     return 0;
     
 }
